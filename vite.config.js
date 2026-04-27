c919049524ac13b1f4eea6b2f177afcdccdedf02
Rui-s0s <segovia.rodrigo2002@gmail.com>
2026-04-27 18:25:45 -0300

    chore: Cambios para recargar sin dependencias de React

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// NEW CONFIG (Vanilla)
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // No plugins needed for standard JS!
+  plugins: [],
 })
